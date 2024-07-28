0062f2d7187cad84299170e2a393c2c4c2394c9d
ashishpandey17360 <144258479+ashishpandey17360@users.noreply.github.com>
2024-07-29 00:34:56 +0530

    update

--- src/Components/Navar.jsx
+++ src/Components/Navar.jsx
@@ -10,7 +10,7 @@ function Navar() {
     <div className="mediaNav">
        <nav className="navbar fixed-top navbar-expand-lg bg-body-tertiary ">
   <div className="container-fluid">
-    <a className="navbar-brand nShift " href="#"> <img src={logo}/> </a>
+    <a className="navbar-brand nShift fw-bold " href="#"> Ashish.dev</a>
     
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
